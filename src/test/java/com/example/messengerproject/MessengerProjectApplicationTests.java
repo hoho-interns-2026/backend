637f6579ad475be75dae6a3d8f23291426b3beee
H-namho <skagh9696@gmail.com>
2026-03-16 13:13:54 +0900

    init Project

---
+++ src/test/java/com/example/messengerproject/MessengerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messengerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
